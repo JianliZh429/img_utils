9cd335c6dfb583a2c89ca6690ccf2400815ea01e
dew.maple <dew.maple@gmail.com>
2018-03-07 12:01:17 +0800

    update to version 0.0.3

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup
 
 setup(
     name='img_utils',
-    version='0.0.2',
+    version='0.0.3',
     packages=['image_utils'],
     description='Convenient functions that help to process images',
     author='dew.maple',
     author_email='dew.maple@gmail.com',
     license='MIT',
     keywords=['computer vision', 'image processing', 'opencv', 'numpy'],
+    classifiers=['Programming Language :: Python :: 3.6'],
+    project_urls={
+        'Bug Reports': 'https://github.com/DewMaple/image_utils/issues',
+        'Source': 'https://github.com/DewMaple/image_utils',
+    }
 )
