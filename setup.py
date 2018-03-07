178d05ac53650a3ca24619865351b6daf8f6f96c
dew.maple <dew.maple@gmail.com>
2018-03-07 11:40:17 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='img_utils',
+    version='0.0.2',
+    packages=['image_utils'],
+    description='Convenient functions that help to process images',
+    author='dew.maple',
+    author_email='dew.maple@gmail.com',
+    license='MIT',
+    keywords=['computer vision', 'image processing', 'opencv', 'numpy'],
+)
